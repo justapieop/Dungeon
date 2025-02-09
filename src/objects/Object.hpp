625a431ea28cd20d669562ec922fd8db08a7d9cf
Do Tran Thai An <justapie06@gmail.com>
2025-02-09 23:01:59 +0700

    fix: fix object not rendering

--- src/objects/Object.hpp
+++ src/objects/Object.hpp
@@ -5,20 +5,22 @@
 #ifndef OBJECT_HPP
 #define OBJECT_HPP
 
-#include <SDL2/SDL.h>
+#include "SDL2/SDL.h"
+#include "string"
 
 class Object {
 public:
-    Object(const char* sheet, SDL_Renderer* renderer);
+    Object(const std::string& sheet, SDL_Renderer* renderer);
     ~Object();
 
-    void set_pos(int x, int y);
+    void set_pos_src(int x, int y);
+    void set_size_src(int w, int h);
+    void set_pos_dest(int x, int y);
+    void set_size_dest(int w, int h);
 
     void update();
     void render() const;
 private:
-    int x;
-    int y;
     SDL_Renderer* renderer;
     SDL_Texture* texture{};
     SDL_Rect source{}, dest{};
