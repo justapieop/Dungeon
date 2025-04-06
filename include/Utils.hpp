f70e63a90bef85e8a47274438edfba05e85ef67a
Do Tran Thai An <justapie06@gmail.com>
2025-04-06 22:58:29 +0700

    feat: add rng method

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -2,8 +2,6 @@
 #define UTILS_HPP
 #include "vector"
 #include "string"
-#include "cmath"
-#include "sstream"
 #include "iomanip"
 
 class Utils
@@ -11,13 +9,8 @@ class Utils
 public:
     static void log_err_and_exit(const char* msg);
     static std::vector<std::vector<int>> load_matrix(const char* path, int m, int n);
-    static std::string round_float(const float f) {
-        const float r = std::round(f * 100.0f) / 100.0f;
-
-        std::ostringstream oss;
-        oss << std::fixed << std::setprecision(2) << r;
-        return oss.str();
-    }
+    static std::string round_float(float f);
+    static float rng(int min, int max);
 };
 
 
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -2,6 +2,7 @@
 #include "SDL2/SDL.h"
 #include "fstream"
 #include "vector"
+#include "sstream"
 
 void Utils::log_err_and_exit(const char* msg) {
     SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s", msg);
@@ -25,4 +26,17 @@ std::vector<std::vector<int>> Utils::load_matrix(const char* path, const int m,
         SDL_Log("Failed to open %s", path);
     }
     return temp;
+}
+
+std::string Utils::round_float(const float f) {
+    const float r = std::round(f * 100.0f) / 100.0f;
+
+    std::ostringstream oss;
+    oss << std::fixed << std::setprecision(2) << r;
+    return oss.str();
+}
+
+float Utils::rng(const int min, const int max) {
+    srand(time(nullptr));
+    return rand() % (max - min + 1) + min;
 }
