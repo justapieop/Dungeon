929a8d23a66616239f66c5d87076ab10a1cfe8e8
Do Tran Thai An <justapie06@gmail.com>
2025-04-22 23:38:39 +0700

    chore: set fps to 60

--- include/Constants.hpp
+++ include/Constants.hpp
@@ -3,7 +3,7 @@
 
 constexpr unsigned int SCREEN_WIDTH = 768;
 constexpr unsigned int SCREEN_HEIGHT = 432;
-constexpr unsigned int FPS = 144;
+constexpr unsigned int FPS = 60;
 constexpr unsigned int MIN_FRAME_TIME = 1000 / FPS;
 constexpr float BASE_DAMAGE = 1.0f;
 
